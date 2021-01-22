8221ce613d74496863117f5f64b7a2c8c9354d27
eder-toledo <me890506@gmail.com>
2021-01-21 21:35:15 -0600

    Adding candidate migration

--- api/database/migrations/1611285528145_candidate_schema.js
+++ api/database/migrations/1611285528145_candidate_schema.js
@@ -4,14 +4,20 @@
 const Schema = use('Schema')
 
 class CandidateSchema extends Schema {
-  up () {
+  up() {
     this.create('candidates', (table) => {
       table.increments()
+      table.string('first_name', 80)
+      table.string('last_name', 80)
+      table.decimal('wish_salary')
+      table.string('title')
+      table.string('phone').unique()
+      table.string('email').unique()
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('candidates')
   }
 }
