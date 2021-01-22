aa5f051bff3cc51f797a989390694855d7fb1708
eder-toledo <me890506@gmail.com>
2021-01-22 13:46:14 -0600

    Basic estructure for distribution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,33 @@
 import LeftMenu from './components/LeftMenu';
+import Footer from './components/Footer';
+import SearchBar from './components/SearchBar';
 import './App.css';
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+
 
 function App() {
   return (
     <div className="App">
-      <LeftMenu />
+      <Grid
+        container
+        spacing={0}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+        alignContent="stretch"
+        wrap="nowrap"
 
+      >
+
+        <Grid item xs={2}>
+          <LeftMenu />
+        </Grid>
+        <Grid>
+          <SearchBar />
+        </Grid>
+      </Grid>
+      <Footer />
     </div>
   );
 }
